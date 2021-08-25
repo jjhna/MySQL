a095c582109ea268b92ef1e345489811c045db87
boorm <jjhna7@gmail.com>
2021-08-25 05:01:25 +0000

    Lessons 5-6: Learned the rest of the CRUD commands, specifically the Read (select), update and delete commands.

--- lessons3-4.sql
+++ lessons3-4.sql
@@ -1,7 +1,46 @@
-mysql>
-mysql> create table employees (id int not null auto_increment, last_name varchar(100) not null, first_name varchar(100) not null, middle_name varchar(100), age int not null, current_status varchar(100) not null defa
-ult 'employed', primary key(id));
+-- When you need to show a list of all the database in the system
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| c9                 |
+| cat_app            |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+6 rows in set (0.03 sec)
+
+-- To create a new database called shirts_db
+mysql> create database shirts_db;
+Query OK, 1 row affected (0.01 sec)
+
+
+-- When you need to use a specific database, in this case we are using the database: cat_app
+-- You must do this before accessing any tables 
+mysql> use cat_app;
+Reading table information for completion of table and column names
+You can turn off this feature to get a quicker startup with -A
+
+-- shows the current database that is being used 
+Database changed
+mysql> select database();
++------------+
+| database() |
++------------+
+| cat_app    |
++------------+
+1 row in set (0.00 sec)
+
+-- Creates a new table with the list of columns and data type that each column will used
+-- also note that you can make each column nullable or not nullable
+-- also you can set the deault value of a column when an input isn't specifiedALTER
+-- also you can set the primary key to a certain column, also the int id is automatically incremented when new data is entered
+mysql> create table employees (id int not null auto_increment, last_name varchar(100) not null, first_name varchar(100) not null, middle_name varchar(100), age int not null, current_status varchar(100) not null default 'employed', primary key(id));
 ERROR 1050 (42S01): Table 'employees' already exists
+
+-- the command to select all the columns from the employees table
 mysql> select * from employees;
 +----+-----------+------------+-------------+-----+----------------+
 | id | last_name | first_name | middle_name | age | current_status |
@@ -10,7 +49,9 @@ mysql> select * from employees;
 +----+-----------+------------+-------------+-----+----------------+
 1 row in set (0.00 sec)
 
+-- adding new data into the employees table specified 3 columns with the 3 added values
 mysql> insert into employees(first_name, last_name, age) values ('Dora', 'explorer', 99);
 Query OK, 1 row affected (0.01 sec)
 
+-- erases or deltes an entire table, in this case the employees table is removed for good.
 mysql> drop table employees;
---
+++ lessons5-6.sql
@@ -0,0 +1,118 @@
+-- The READ FROM CRUD:
+
+mysql> select cat_id, name, age, breed from cats;
++--------+----------------+------+------------+
+| cat_id | name           | age  | breed      |
++--------+----------------+------+------------+
+|      1 | Ringo          |    4 | Tabby      |
+|      2 | Cindy          |   10 | Maine Coon |
+|      3 | Dumbledore     |   11 | Maine Coon |
+|      4 | Egg            |    4 | Persian    |
+|      5 | Misty          |   13 | Tabby      |
+|      6 | George Michael |    9 | Ragdoll    |
+|      7 | Jackson        |    7 | Sphynx     |
++--------+----------------+------+------------+
+7 rows in set (0.00 sec)
+
+mysql> select * from cats where age=4;
++--------+-------+---------+------+
+| cat_id | name  | breed   | age  |
++--------+-------+---------+------+
+|      1 | Ringo | Tabby   |    4 |
+|      4 | Egg   | Persian |    4 |
++--------+-------+---------+------+
+2 rows in set (0.01 sec)
+
+-- Aliases = printing out a column from a data as a different name ex:
+
+mysql> select cat_id as id,name from cats;
++----+----------------+
+| id | name           |
++----+----------------+
+|  1 | Ringo          |
+|  2 | Cindy          |
+|  3 | Dumbledore     |
+|  4 | Egg            |
+|  5 | Misty          |
+|  6 | George Michael |
+|  7 | Jackson        |
++----+----------------+
+7 rows in set (0.00 sec)
+
+mysql> select name as 'cat name', breed as 'kitty breed' from cats;
++----------------+-------------+
+| cat name       | kitty breed |
++----------------+-------------+
+| Ringo          | Tabby       |
+| Cindy          | Maine Coon  |
+| Dumbledore     | Maine Coon  |
+| Egg            | Persian     |
+| Misty          | Tabby       |
+| George Michael | Ragdoll     |
+| Jackson        | Sphynx      |
++----------------+-------------+
+7 rows in set (0.00 sec)
+
+-- The Update from CRUD:
+-- Note also look at the breed tabby from the previous table that is listed above.
+
+-- NOTE this is WHERE becomes important to both the update and delete CRUDS because you need to specify which 
+
+mysql> update cats set breed='shorthair'
+    -> where breed='tabby';
+Query OK, 2 rows affected (0.02 sec)
+Rows matched: 2  Changed: 2  Warnings: 0
+
+mysql> select * from cats;
++--------+----------------+------------+------+
+| cat_id | name           | breed      | age  |
++--------+----------------+------------+------+
+|      1 | Ringo          | shorthair  |    4 |
+|      2 | Cindy          | Maine Coon |   10 |
+|      3 | Dumbledore     | Maine Coon |   11 |
+|      4 | Egg            | Persian    |    4 |
+|      5 | Misty          | shorthair  |   13 |
+|      6 | George Michael | Ragdoll    |    9 |
+|      7 | Jackson        | Sphynx     |    7 |
++--------+----------------+------------+------+
+7 rows in set (0.00 sec)
+
+mysql> update cats set age=14 where name = 'misty';
+Query OK, 1 row affected (0.01 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> select * from cats;
++--------+----------------+------------+------+
+| cat_id | name           | breed      | age  |
++--------+----------------+------------+------+
+|      1 | Ringo          | shorthair  |    4 |
+|      2 | Cindy          | Maine Coon |   10 |
+|      3 | Dumbledore     | Maine Coon |   11 |
+|      4 | Egg            | Persian    |    4 |
+|      5 | Misty          | shorthair  |   14 |
+|      6 | George Michael | Ragdoll    |    9 |
+|      7 | Jackson        | Sphynx     |    7 |
++--------+----------------+------------+------+
+
+-- The Delete from CRUD
+-- This is both deleteing a data entry from a table
+
+mysql> delete from cats where name='egg';
+Query OK, 1 row affected (0.02 sec)
+
+mysql> select * from cats;
++--------+----------------+-------------------+------+
+| cat_id | name           | breed             | age  |
++--------+----------------+-------------------+------+
+|      1 | Ringo          | british shorthair |    4 |
+|      2 | Cindy          | Maine Coon        |   12 |
+|      3 | Dumbledore     | Maine Coon        |   12 |
+|      5 | Misty          | shorthair         |   14 |
+|      6 | George Michael | Ragdoll           |    9 |
+|      7 | jack           | Sphynx            |    7 |
++--------+----------------+-------------------+------+
+6 rows in set (0.00 sec)
+
+-- note that this will delete all the data from the cats table, HOWEVER the table still remains
+Delete from cats;
+
